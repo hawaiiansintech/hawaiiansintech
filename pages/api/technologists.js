40d0896845f5cffa2bbfe433f7c649a7692e3a33
Taylor Ho <taylorkmho@gmail.com>
2021-07-25 11:50:31 -1000

    Using staging spreadsheet

--- pages/api/technologists.js
+++ pages/api/technologists.js
@@ -1,7 +1,10 @@
 import drive from "drive-db";
 
 export default async (req, res) => {
-  const db = await drive("1W7cOTU2nUfT-3N88Aa09ZtZYv_KhtYkbCtpdm_9FeT4");
+  // TODO: Before mergin
+  // Swap back from staging spreadsheet
+  // const db = await drive("1W7cOTU2nUfT-3N88Aa09ZtZYv_KhtYkbCtpdm_9FeT4");
+  const db = await drive("1Yq9XC-c_tuTiB4MrKtw1p6HRAr9MTDYa9iXybaytk1U");
   let sanitizeResult = db.filter(
     (item) => item.name != "" && item.exclude !== "Yes"
   );
