ebcd11a0c71e771189f081f3a688abbe01297d30
emmit-parubrub <eparubrub@dons.usfca.edu>
2021-08-29 22:19:47 -0700

    first draft of main member spotlight page

--- components/Nav.js
+++ components/Nav.js
@@ -13,6 +13,11 @@ export default function Nav(props) {
           <a>Nominate</a>
         </Link>
       </li>
+      <li>
+        <Link href="/spotlights">
+          <a>Spotlights</a>
+        </Link>
+      </li>
 
       <style jsx>{`
         ul {
--- pages/spotlights.js
+++ pages/spotlights.js
@@ -1,42 +1,27 @@
 import Head from "next/head";
 import Link from "next/link";
-import { motion } from "framer-motion";
-import Title from "../components/Title.js";
 import MetaTags from "../components/Metatags.js";
-import Nav from "../components/Nav.js";
 import HitLogo from "../components/HitLogo.js";
 import MainSpotlight from "../components/Spotlight/MainSpotlight.js"
 import React from "react"; 
 
-const item = {
-  hidden: { opacity: 0, y: "15%" },
-  show: { opacity: 1, y: "0%" },
-  transition: {
-    easing: "easeInOut",
-  },
-};
 
-const container = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.15,
-    },
-  },
-};
-
-const DISCORD_URL = "https://discord.gg/p7338Z5MJQ";
-
-export default function Home({ technologists }) {
+export default function Home() {
   return (
-    <React.Fragment>
       <div className="container">
-          <HitLogo />
-          <Nav />
-          <MainSpotlight />
-        </div>
-    </React.Fragment>
+        <Head>
+          <title>Hawaiians in Technology | About</title>
+          <link rel="icon" href="/favicon.ico" />
+          <MetaTags />
+        </Head>
 
+        <Link href="/" shallow={true}>
+          <a className="auxNav arrowback">←</a>
+        </Link>
+
+        <HitLogo />
+        <MainSpotlight />
+
+      </div>
   );
 }
