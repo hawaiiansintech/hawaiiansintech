9556efea33073bb4c6858e92a8e8761e4c364da7
Taylor Ho <taylorkmho@gmail.com>
2021-10-17 18:17:00 -0700

    using globalStyles instead of css file

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
-import "../assets/styles/modernism.css";
+import globalStyles from "../styles/global";
 import { AnimatePresence } from "framer-motion";
 
 function App({ Component, pageProps }) {
   return (
-    <AnimatePresence>
-      <Component {...pageProps} />
-    </AnimatePresence>
+    <>
+      <AnimatePresence>
+        <Component {...pageProps} />
+      </AnimatePresence>
+      <style jsx global>
+        {globalStyles}
+      </style>
+    </>
   );
 }
 
