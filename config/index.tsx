2901289d09f99684752af8269330e165716ef83e
Taylor Ho <taylorkmho@gmail.com>
2022-01-11 12:11:09 -1000

    using staging environment

--- config/index.tsx
+++ config/index.tsx
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://hawaiiansintech.org";
+  : "https://hawaiiansintech-git-tho-join-page-hawaiians.vercel.app";
