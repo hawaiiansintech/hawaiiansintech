490ab0af4960f78e3dd854b4e8ec15dc4be895f0
Andrew Taeoalii <ataeoalii@gmail.com>
2022-02-19 13:48:14 -1000

    Add Google Analytics
    - Add GA script to app.js
    - Create stub API for easy testing without airtable API keys
    
    TODO: Actual GA account needs to be setup for the project,
    
    [x] - tested locally
    [x] - setup personal GA account
    [x] - captured standard site visit, page, event stats

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import "../assets/styles/modernism.css";
 import { AnimatePresence } from "framer-motion";
+import Script from "next/script";
 
 function App({ Component, pageProps }) {
   return (
-    <AnimatePresence>
-      <Component {...pageProps} />
-    </AnimatePresence>
+      <AnimatePresence>
+      {/* Adds google analytics to all pages */}
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TAG_ID}`} />
+      <Script strategy='lazyOnload'>
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GA_TAG_ID}');
+          `
+        }
+      </Script>
+        <Component {...pageProps} />
+      </AnimatePresence>
   );
 }
 
