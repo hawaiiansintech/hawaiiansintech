9f03720eb0e477a18645eb97c762845df4ca565f
Taylor Ho <taylorkmho@gmail.com>
2023-06-13 01:53:25 -0700

    tailwind actually working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  mode: "jit",
+  purge: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          50: "#fef9ec",
+          100: "#fbeeca",
+          200: "#f6dc91",
+          300: "#f1c358",
+          400: "#eead31",
+          500: "#e68d1a",
+          600: "#cb6a13",
+          700: "#a94b14",
+          800: "#8a3b16",
+          900: "#713116",
+          950: "#411707",
+        },
+      },
+    },
   },
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 };
