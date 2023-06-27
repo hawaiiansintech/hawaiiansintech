f4fa3e0d9181b8c2d2e0ce58dbd8d0a62c460bcd
Taylor Ho <taylorkmho@gmail.com>
2023-06-26 23:34:46 -0700

    using @layer directive for base styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import { AnimatePresence } from "framer-motion";
-import { useEffect } from "react";
 import globalStyles from "../styles/global";
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
-  useEffect(() => {
-    document.body.className = "bg-tan-200";
-  });
   return (
     <>
       <AnimatePresence>
