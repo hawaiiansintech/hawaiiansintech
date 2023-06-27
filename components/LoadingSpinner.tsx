3252159686e35470b8b3cdd17e43a7a8bb8fe0a2
Taylor Ho <taylorkmho@gmail.com>
2023-06-27 00:05:05 -0700

    Tech debt, removing old and refactoring new

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,39 +1,16 @@
-export enum LoadingSpinnerColor {
-  Brand = "var(--color-brand)",
-  Black = "var(--color-text-alt)",
-  Overlay = "var(--color-text-overlay)",
-}
-
-export enum LoadingSpinnerSize {
-  Small = "1rem",
-  Medium = "2rem",
-  Large = "4rem",
-}
-
-interface LoadingSpinnerProps {
-  color?: LoadingSpinnerColor;
-  size?: LoadingSpinnerSize;
-}
-
-export default function LoadingSpinner({
-  color = LoadingSpinnerColor.Overlay,
-  size = LoadingSpinnerSize.Medium,
-}: LoadingSpinnerProps) {
+export default function LoadingSpinner() {
   return (
-    <figure className="loading-spinner">
-      <style jsx>{`
-        figure {
-          display: inline-block;
-          width: ${size};
-          height: ${size};
-          margin: 0;
-          border: calc(${size} / 8) solid rgba(255, 255, 255, 0.5);
-          border-radius: 50%;
-          border-top-color: ${color};
-          animation: spin 1s ease-in-out infinite;
-          -webkit-animation: spin 1s ease-in-out infinite;
-        }
-      `}</style>
-    </figure>
+    <figure
+      className={`
+        inline-block
+        h-6
+        w-6
+        animate-spin
+        rounded-full
+        border-4
+        border-white/50
+        border-t-white
+      `}
+    ></figure>
   );
 }
