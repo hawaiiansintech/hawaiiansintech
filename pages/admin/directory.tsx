91cf0aaa3bcfd4fc792c2f5b23058080262f2635
Taylor Ho <taylorkmho@gmail.com>
2023-09-24 18:46:25 -0700

    got old admin working!

--- pages/admin/directory.tsx
+++ pages/admin/directory.tsx
@@ -3,14 +3,15 @@ import Button, { ButtonVariant } from "@/components/Button";
 import ErrorMessage, {
   ErrorMessageProps,
 } from "@/components/form/ErrorMessage";
+import Input from "@/components/form/Input";
 import LoadingSpinner, {
   LoadingSpinnerVariant,
 } from "@/components/LoadingSpinner";
 import MetaTags from "@/components/Metatags";
 import Modal from "@/components/Modal";
 import Plausible from "@/components/Plausible";
-import Tabs from "@/components/Tabs";
 import Tag, { TagVariant } from "@/components/Tag";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   deleteDocument,
   deleteReferences,
@@ -25,6 +26,7 @@ import {
 import { FirebaseTablesEnum, StatusEnum } from "@/lib/enums";
 import { useUserSession } from "@/lib/hooks";
 import { cn, convertStringSnake } from "helpers";
+import { Trash } from "lucide-react";
 import moment from "moment";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -90,7 +92,6 @@ export default function DirectoryPage(props: {
           isAdmin={isAdmin}
           isLoggedIn={isLoggedIn}
           name={userData?.name}
-          sticky
         />
         <Admin.Body>
           {userData === null && (
@@ -162,15 +163,20 @@ const Directory: MemberDirectoryType = ({ members }) => {
           <div className="flex grow items-center gap-2">
             <h2 className="text-xl font-semibold leading-8">Directory</h2>
             <Tabs
-              items={Object.values(DirectoryFilter).map((filter) => ({
-                label: filter,
-                onClick: () => {
-                  if (tabVisible === filter) return;
-                  setTabVisible(filter);
-                },
-                selected: tabVisible === filter,
-              }))}
-            />
+              defaultValue={Object.values(DirectoryFilter)[0]}
+              onValueChange={(value) => {
+                setTabVisible(value as DirectoryFilter);
+              }}
+              value={tabVisible}
+            >
+              <TabsList loop>
+                {Object.values(DirectoryFilter).map((filter, i) => (
+                  <TabsTrigger value={filter} key={`directory-filter-${i}`}>
+                    {filter}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+            </Tabs>
             <div className="flex grow justify-end">
               <select
                 className="rounded px-1 py-0.5 text-sm"
@@ -257,7 +263,6 @@ function Card({ member, isHidden, setIsHidden }: CardProps) {
         className={cn(
           `
             group
-            w-full
             border-b
           `,
           member.status === StatusEnum.APPROVED
@@ -323,12 +328,12 @@ function Card({ member, isHidden, setIsHidden }: CardProps) {
               </div>
               <div className="flex items-center gap-2">
                 <h3 className="text-xl font-semibold">{member.name}</h3>
-                <h3 className="text-sm font-light text-stone-500 group-hover:text-stone-600">
+                <h3 className="text-sm font-light text-secondary-foreground">
                   {member.id}
                 </h3>
               </div>
               <span
-                className="text-xs font-light text-stone-500"
+                className="text-xs font-light text-secondary-foreground"
                 title={`${member.lastModified}`}
               >
                 Last modified{" "}
@@ -344,31 +349,31 @@ function Card({ member, isHidden, setIsHidden }: CardProps) {
               )}
             >
               <section>
-                <h4 className="font-medium text-stone-600">Title</h4>
-                <p className="break-words font-light text-stone-500">
+                <h4 className="font-medium">Title</h4>
+                <p className="break-words font-light text-secondary-foreground">
                   {member.title}
                 </p>
               </section>
               <section>
-                <h4 className="font-medium text-stone-600">Location</h4>
-                <p className="break-words font-light text-stone-500">
+                <h4 className="font-medium">Location</h4>
+                <p className="break-words font-light text-secondary-foreground">
                   {member.location}
                 </p>
               </section>
               <section>
-                <h4 className="font-medium text-stone-600">Region</h4>
-                <p className="break-words font-light text-stone-500">
+                <h4 className="font-medium">Region</h4>
+                <p className="break-words font-light text-secondary-foreground">
                   {member.region}
                 </p>
               </section>
               <section>
-                <h4 className="font-medium text-stone-600">Company Size</h4>
-                <p className="break-words font-light text-stone-500">
+                <h4 className="font-medium">Company Size</h4>
+                <p className="break-words font-light text-secondary-foreground">
                   {member.companySize}
                 </p>
               </section>
               <section>
-                <h4 className="font-medium text-stone-600">Focuses</h4>
+                <h4 className="font-medium">Focuses</h4>
                 <p>
                   {member.focus &&
                     member.focus.map((focus, i) => {
@@ -377,7 +382,7 @@ function Card({ member, isHidden, setIsHidden }: CardProps) {
                       return (
                         <span
                           className={cn(
-                            "font-light text-stone-500",
+                            "font-light text-secondary-foreground",
                             focusNotApproved && `font-medium text-violet-600`
                           )}
                           key={member.id + focus.id}
@@ -391,7 +396,7 @@ function Card({ member, isHidden, setIsHidden }: CardProps) {
                 </p>
               </section>
               <section>
-                <h4 className="font-medium text-stone-600">Industries</h4>
+                <h4 className="font-medium">Industries</h4>
                 <p>
                   {member.industry &&
                     member.industry.map((industry, i) => {
@@ -400,7 +405,7 @@ function Card({ member, isHidden, setIsHidden }: CardProps) {
                       return (
                         <span
                           className={cn(
-                            "font-light text-stone-500",
+                            "font-light text-secondary-foreground",
                             industryNotApproved && `font-medium text-violet-600`
                           )}
                           key={member.id + industry.id}
@@ -431,12 +436,12 @@ const MemberProfileModal: FC<{
   const [isDeleting, setIsDeleting] = useState(false);
 
   return (
-    <div className="pointer-events-none fixed inset-x-0 inset-y-0 z-50">
-      <div className="pointer-events-auto absolute inset-x-0 bottom-0 flex flex-col gap-4 rounded-t-xl bg-white p-4 text-center shadow-lg sm:m-4 sm:rounded-b-xl">
+    <div className="fixed inset-x-0 inset-y-0 z-50 flex items-center justify-center bg-white/50">
+      <div className="pointer-events-auto flex max-w-5xl flex-col gap-4 rounded-t-xl bg-white p-4 text-center shadow-lg sm:m-4 sm:rounded-b-xl">
         {isDeleting ? (
           <>
             <h2 className="text-2xl font-semibold">Delete Member</h2>
-            <p className="text-xl font-light text-stone-500">
+            <p className="text-xl font-light text-secondary-foreground">
               Are you sure you want to delete{" "}
               <span className="font-semibold text-stone-700">
                 {member.name}
@@ -464,9 +469,15 @@ const MemberProfileModal: FC<{
           </>
         ) : (
           <>
-            <div className="px-4 pt-4 text-center">
-              <h2 className="text-2xl font-semibold">{member.name}</h2>
+            <div className="flex items-center gap-2 pt-4">
+              <h2 className="text-sm font-semibold">Name:</h2>
+              <Input name={"usernamef"} value={member.name} />
             </div>
+            <div className="flex items-center gap-2 pt-4">
+              <h2 className="text-sm font-semibold">Title:</h2>
+              <Input name={"title"} value={member.title} />
+            </div>
+            <Tabs />
             <div
               className={cn(
                 `grid
@@ -485,29 +496,25 @@ const MemberProfileModal: FC<{
               )}
             >
               <section>
-                <h4 className="text-base font-semibold text-stone-600">ID</h4>
-                <p className="text-base font-light text-stone-500">
+                <h4 className="text-base font-semibold">ID</h4>
+                <p className="text-base font-light text-secondary-foreground">
                   {member.id}
                 </p>
               </section>
               <section>
-                <h4 className="text-base font-semibold text-stone-600">Name</h4>
-                <p className="text-base font-light text-stone-500">
+                <h4 className="text-base font-semibold">Name</h4>
+                <p className="text-base font-light text-secondary-foreground">
                   {member.name}
                 </p>
               </section>
               <section>
-                <h4 className="text-base font-semibold text-stone-600">
-                  Title
-                </h4>
-                <p className="text-base font-light text-stone-500">
+                <h4 className="text-base font-semibold">Title</h4>
+                <p className="text-base font-light text-secondary-foreground">
                   {member.title}
                 </p>
               </section>
               <section>
-                <h4 className="text-base font-semibold text-stone-600">
-                  Status
-                </h4>
+                <h4 className="text-base font-semibold">Status</h4>
                 <Tag
                   variant={
                     member.status === StatusEnum.APPROVED
@@ -523,41 +530,31 @@ const MemberProfileModal: FC<{
                 </Tag>
               </section>
               <section>
-                <h4 className="text-base font-semibold text-stone-600">
-                  Last Modified
-                </h4>
-                <p className="text-base font-light text-stone-500">
+                <h4 className="text-base font-semibold">Last Modified</h4>
+                <p className="text-base font-light text-secondary-foreground">
                   {member.lastModified}
                 </p>
               </section>
               <section>
-                <h4 className="text-base font-semibold text-stone-600">
-                  Location
-                </h4>
-                <p className="text-base font-light text-stone-500">
+                <h4 className="text-base font-semibold">Location</h4>
+                <p className="text-base font-light text-secondary-foreground">
                   {member.location}
                 </p>
               </section>
               <section>
-                <h4 className="text-base font-semibold text-stone-600">
-                  Region
-                </h4>
-                <p className="text-base font-light text-stone-500">
+                <h4 className="text-base font-semibold">Region</h4>
+                <p className="text-base font-light text-secondary-foreground">
                   {member.region}
                 </p>
               </section>
               <section>
-                <h4 className="text-base font-semibold text-stone-600">
-                  Company Size
-                </h4>
-                <p className="text-base font-light text-stone-500">
+                <h4 className="text-base font-semibold">Company Size</h4>
+                <p className="text-base font-light text-secondary-foreground">
                   {member.companySize}
                 </p>
               </section>
               <section>
-                <h4 className="text-base font-semibold text-stone-600">
-                  Focuses
-                </h4>
+                <h4 className="text-base font-semibold">Focuses</h4>
                 <p>
                   {member.focus &&
                     member.focus.map((focus, i) => {
@@ -566,7 +563,7 @@ const MemberProfileModal: FC<{
                       return (
                         <span
                           className={cn(
-                            "text-base font-light text-stone-500",
+                            "text-base font-light text-secondary-foreground",
                             focusNotApproved && `font-medium text-violet-600`
                           )}
                           key={member.id + focus.id}
@@ -580,9 +577,7 @@ const MemberProfileModal: FC<{
                 </p>
               </section>
               <section>
-                <h4 className="text-base font-semibold text-stone-600">
-                  Industries
-                </h4>
+                <h4 className="text-base font-semibold">Industries</h4>
                 <p>
                   {member.industry &&
                     member.industry.map((industry, i) => {
@@ -591,7 +586,7 @@ const MemberProfileModal: FC<{
                       return (
                         <span
                           className={cn(
-                            "text-base font-light text-stone-500",
+                            "text-base font-light text-secondary-foreground",
                             industryNotApproved && `font-medium text-violet-600`
                           )}
                           key={member.id + industry.id}
@@ -607,9 +602,13 @@ const MemberProfileModal: FC<{
                 </p>
               </section>
             </div>
-            <div className="flex flex-col justify-between gap-2 sm:flex-row">
-              <Button variant={ButtonVariant.Primary} onClick={onClose}>
-                Modify
+            <div className="flex flex-col gap-2 sm:flex-row">
+              <Button
+                disabled
+                variant={ButtonVariant.Primary}
+                onClick={onClose}
+              >
+                Save
               </Button>
               <Button
                 variant={ButtonVariant.Destructive}
@@ -617,11 +616,13 @@ const MemberProfileModal: FC<{
                   setIsDeleting(true);
                 }}
               >
-                Remove
+                <Trash />
               </Button>
-              <Button variant={ButtonVariant.Secondary} onClick={onClose}>
-                Close
-              </Button>
+              <div className="flex grow justify-end">
+                <Button variant={ButtonVariant.Secondary} onClick={onClose}>
+                  Close
+                </Button>
+              </div>
             </div>
           </>
         )}
