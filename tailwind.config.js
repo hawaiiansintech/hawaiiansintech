d43e60e1f83749e058e2bd4a56a6bf4e3d8d11fb
Taylor Ho <taylorkmho@gmail.com>
2023-10-06 21:02:43 -0700

    refactor tailwind to shadcn/ui approach

---
+++ tailwind.config.js
@@ -0,0 +1,111 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  mode: "jit",
+  purge: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    fontFamily: {
+      script: ["Permanent Marker"],
+    },
+    extend: {
+      borderWidth: {
+        6: "6px",
+      },
+      colors: {
+        brown: {
+          50: "#fef9ec",
+          100: "#fbeeca",
+          200: "#f6dc91",
+          300: "#f1c358",
+          400: "#eead31",
+          500: "#e68d1a",
+          600: "#cb6a13",
+          700: "#a94b14",
+          800: "#8a3b16",
+          900: "#713116",
+          950: "#411707",
+        },
+        tan: {
+          50: "#f9f7f7",
+          100: "#f3eeed",
+          200: "#e9e1df",
+          300: "#d9cbc8",
+          400: "#c9b5b1",
+          500: "#ab8e88",
+          600: "#93756f",
+          700: "#7a605b",
+          800: "#67514d",
+          900: "#584744",
+          950: "#2d2422",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        borderRadius: {
+          lg: "var(--radius)",
+          md: "calc(var(--radius) - 2px)",
+          sm: "calc(var(--radius) - 4px)",
+        },
+        keyframes: {
+          "accordion-down": {
+            from: { height: 0 },
+            to: { height: "var(--radix-accordion-content-height)" },
+          },
+          "accordion-up": {
+            from: { height: "var(--radix-accordion-content-height)" },
+            to: { height: 0 },
+          },
+        },
+        animation: {
+          "accordion-down": "accordion-down 0.2s ease-out",
+          "accordion-up": "accordion-up 0.2s ease-out",
+        },
+      },
+    },
+  },
+};
