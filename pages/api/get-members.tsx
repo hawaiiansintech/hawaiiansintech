962f9bceb594fdd1f80c6b4ced2e2c2dd36c9b1b
Taylor Ho <taylorkmho@gmail.com>
2023-10-15 23:08:03 -0700

    passing status as a query param works

--- pages/api/get-members.tsx
+++ pages/api/get-members.tsx
@@ -1,8 +1,17 @@
 import { getMembers } from "@/lib/api";
+import { StatusEnum } from "@/lib/enums";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
     return res.status(405).json({ message: "Only GET requests allowed" });
+  }
+
+  if (
+    req.query.status &&
+    !Object.values(StatusEnum).includes(req.query.status)
+  ) {
+    res.status(400).json({ error: "Invalid status parameter" });
+    return;
   }
 
   try {
@@ -12,7 +21,7 @@ export default async function handler(req, res) {
       // TODO add back in when we have a use case for it
       // industries,
       // focuses
-    ] = await getMembers();
+    ] = await getMembers(undefined, undefined, undefined, req.query.status);
     return res.status(200).json({
       message: "Successfully fetched members.",
       members: members,
